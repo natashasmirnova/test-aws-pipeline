b660052a4a36e2d23a214b1a96ff86711fa06e4f
Natasha Smirnova <Natasha.Smirnova@TradeMe.local>
2017-12-21 21:42:30 +1300

    some changes

---
+++ Controllers/HealthCheckController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace netcore_api.Controllers
+{
+    [Route("[controller]")]
+    [Route("")]
+    public class HealthCheckController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world";
+        }
+
+        [HttpGet("helloworld")]
+        public string HelloWorld()
+        {
+            return "Hello world";
+        }
+    }
+}
